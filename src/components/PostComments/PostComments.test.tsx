f4d102840cb43b81e87a42a2a9689be57c84a69f
Bruno Santos <rapazcomumvl@hotmail.com>
2023-05-04 10:08:08 -0300

    Teste de adição de 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+
+    test('Deve adicionar 2 comentários', () => {
+        const { debug } = render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value:'Realmente, ele é muito legal!!!'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-adicionar'))
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Tenho uma miniatura do superman'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-adicionar'))
+        debug()
+
+        expect(screen.getByText('Realmente, ele é muito legal!!!')).toBeInTheDocument()
+        expect(screen.getByText('Tenho uma miniatura do superman')).toBeInTheDocument()
+    })
 });
